c6dd7ab697a3904c46425de27325c66f0d9cce9d
Monalicous <samukelisiwentusi@outlook.com>
2024-12-06 11:14:54 +0400

    Added a funtion to get multiple city time zone

--- index.js
+++ index.js
@@ -1,21 +1,49 @@
-setInterval(function () {
-    let cityOne = document.querySelector("#mwanza");
-    let cityOneDate = cityOne.querySelector(".date") // code says find the date element in tanzania
-    let cityOneTime = cityOne.querySelector(".time"); // find time element
-    let cityMwanza = moment().tz("Tanzania Mwanza");
+function updateTime() {
+    let cityElement = document.querySelector("#auckland");
+    if (cityElement) {
+        let cityOneDate = cityElement.querySelector(".date") // code says find the date element in tanzania
+        let cityOneTime = cityElement.querySelector(".time"); // find time element
+        let cityMwanza = moment().tz("Pacific/Auckland");
 
-    cityOneDate.innerHTML = cityMwanza.format("MMMM Do YYYY");
+        cityOneDate.innerHTML = cityMwanza.format("MMMM Do YYYY");
 
-    cityOneTime.innerHTML = cityMwanza.format("h:mm:ss[ <small>]A[</small>]");
+        cityOneTime.innerHTML = cityMwanza.format("h:mm:ss[ <small>]A[</small>]");
+
+    }
+
+    let cityTwoElement = document.querySelector("#ladysmith");
+    if (cityTwoElement) {
+        let cityTwoDate = cityTwoElement.querySelector(".date") // code says find the date element in tanzania
+        let cityTwoTime = cityTwoElement.querySelector(".time"); // find time element
+        let cityLagos = moment().tz("South Africa/Ladysmith");
+
+        cityTwoDate.innerHTML = cityLagos.format("MMMM Do YYYY");
+        cityTwoTime.innerHTML = cityLagos.format("h:mm:ss [<small>]A[</small>]");
+    }
+
+};
+
+function updateCity(event) {
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = cityTimeZone;
+
+    citiesElement.innerHTML = `
+     <div class="city">
+                <div>
+                    <h2>${cityName}</h2>
+                    <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+                </div>
+                <div class="time">${cityTime.format("h:mm:ss")}<small> ${cityTime.format("A")}</small></div>
+                </div>` ;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
 
 
-    let cityTwo = document.querySelector("#lagos");
-    let cityTwoDate = cityTwo.querySelector(".date") // code says find the date element in tanzania
-    let cityTwoTime = cityTwo.querySelector(".time"); // find time element
-    let cityLagos = moment().tz("Nigeria Lagos");
+let citiSelect = document.querySelector("#citi");
 
-    cityTwoDate.innerHTML = cityLagos.format("MMMM Do YYYY");
-    cityTwoTime.innerHTML = cityLagos.format("h:mm:ss [<small>]A[</small>]");
-}, 1000);
-
-
+citiSelect.addEventListener("change", updateCity);
